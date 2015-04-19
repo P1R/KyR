8e9807eaf80d461ec3cd4e8e4081a91283854f85
Alex Cronos <alexmadden07@gmail.com>
2015-04-18 21:46:42 -0500

    Removing Unnecessary things

--- Chapter-1/excercise-1-1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main()
-{
-    printf("Hello, ")
-    printf("world";
-    printf("\n);
-    printf("Hello, world");
-}
--- Chapter-1/excercise-1-7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* copy input to output; 2nd version */
-main()
-{
-    int c;
-
-    printf("%d",c = EOF);
-}
